9c8dadab6873cd24b05cb9d5b26a7c533211d618
ethomps33 <e.thomps33@gmail.com>
2023-05-06 14:30:17 -0700

    Added code to add toruque to the character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
+    //Member variables
+    Rigidbody2D rb2d;
+    [SerializeField]float tAmount = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+     //Setting the rigidbody of the character to rb2d   
+     rb2d = GetComponent<Rigidbody2D>(),   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if the Left Arrow us pressed torque will be applied to the character
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            rb2d.AddTorque(tAmount);
+        }
     }
 }
